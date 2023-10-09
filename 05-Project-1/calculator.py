f7c1e32d2be99a1aa8fd4b128d444a10bf5617f4
Cooper Warren Cleveland <Cooper.Cleveland912@gmail.com>
2023-10-09 16:26:55 -0500

    finished calculator

--- 05-Project-1/calculator.py
+++ 05-Project-1/calculator.py
@@ -1,9 +1,9 @@
 people = int(input("number in party"))
-cost = int(input("meal cost"))
-t1 = "$" + str(round(cost * float("1.15")))
-t2 = t1.format("1.20")
-t3 = t1.format("1.25")
-if people > 8:
+cost = float(input("meal cost"))
+t1 = "$" + str(round(cost * 1.15))
+t2 = "$" + str(round(cost * 1.20))
+t3 = "$" + str(round(cost * 1.25))
+if people >= 8:
     print("For parties greater than 8 a 20% tip is automatically added, your total is " + t2)
 else:
     print("tip options are:")
